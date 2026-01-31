48dff6bc815b5988cf11afd87e848cb6a6d78b3d
Will Berg <willberg510@gmail.com>
2026-01-30 23:38:24 -0500

    Set up website with Vite

---
+++ willberg-dot-com/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>WELCOME TO THE WILL BERG WEBSITE</h1>
+      <h3>THE PLACE TO BE</h3>
+      <h5>Glad you could make it</h5>
+    </>
+  )
+}
+
+export default App
