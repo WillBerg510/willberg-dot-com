68537fa7a4fe90e67f4e75c77de1616cb2f7ed51
Will Berg <willberg510@gmail.com>
2026-03-15 22:41:00 -0400

    Test if BRANCH environment variable exists in deployment

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,3 +1,5 @@
+console.log(import.meta.env.BRANCH);
+
 export const BACKEND = import.meta.env.VITE_BACKEND_PORT
   ? `http://${window.location.hostname}:${import.meta.env.VITE_BACKEND_PORT}`
   : `https://${import.meta.env.BRANCH && import.meta.env.BRANCH != "main" ? "branch" : ""}server.willbergforever.com`;
